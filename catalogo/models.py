f1844c31b2b972177cd28f3734bf5e62ce7c2e97
lucasgsouza03 <30882839+lucasgsouza03@users.noreply.github.com>
2018-11-14 22:54:58 -0200

    Add files via upload

--- catalogo/models.py
+++ catalogo/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 class Categoria(models.Model):
     nome = models.CharField('Nome', max_length=100)
-    slug = models.SlugField('Identificados', max_length=100, unique=True)
+    slug = models.SlugField('Identificados', max_length=100)
     criado = models.DateTimeField('Criado em', auto_now_add=True)
     modificado = models.DateTimeField('Modificado em', auto_now=True)
 
@@ -23,9 +23,8 @@ class Categoria(models.Model):
 
 class Produto(models.Model):
     nome = models.CharField('Nome', max_length=100)
-    slug = models.SlugField('Identificador', max_length=100, unique=True)
+    slug = models.SlugField('Identificados', max_length=100)
     categoria = models.ForeignKey('catalogo.Categoria', verbose_name='Categoria', on_delete=models.CASCADE)
-
     descricao = models.TextField('Descrição', blank=True)
     price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
     criado = models.DateTimeField('Criado em', auto_now_add=True)
@@ -41,19 +40,3 @@ class Produto(models.Model):
     
     def get_store_url(self):
         return reverse('loja_produto', kwargs={'slug':self.slug})
-
-class Materia(models.Model):
-    nome = models.CharField('Nome', max_length=100)
-    slug = models.SlugField('Identificados', max_length=100, unique=True)
-    descricao = models.TextField('Descrição', blank=True)
-    criado = models.DateTimeField('Criado em', auto_now_add=True)
-    modificado = models.DateTimeField('Modificado em', auto_now=True)
-    
-    class Meta:
-        ordering = ['nome']
-
-    def __str__(self):
-        return self.nome
-    
-    def get_absolute_url(self):
-        return reverse('catalogo:materia_detalhes', kwargs={'slug':self.slug}) 
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from src.usuario import Gerencia_permissao
 from django.contrib.auth.decorators import login_required, user_passes_test
-from .models import Produto, Categoria, Materia
-from .forms import *
-from src.catalogo import Gerencia_categoria, Gerencia_produto, Gerencia_materia
+from .models import Produto, Categoria
+from src.catalogo import Gerencia_categoria, Gerencia_produto
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -13,12 +12,12 @@ def check_catalogo(request):
     return 'CATALOGO' in Gerencia_permissao.Pega_grupo(request)
 
 @login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
+@user_passes_test(check_catalogo, login_url='erro_acesso', redirect_field_name=None)
 def catalogo(request):
     return render(request, "catalogo.html")
 
 @login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
+@user_passes_test(check_catalogo, login_url='erro_acesso', redirect_field_name=None)
 def categorias(request):
     contexto = {
         'categorias': Categoria.objects.all()
@@ -29,22 +28,16 @@ def categorias(request):
     return render(request, "categorias.html", contexto)
 
 @login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
+@user_passes_test(check_catalogo, login_url='erro_acesso', redirect_field_name=None)
 def adiciona_categoria(request):
     if request.method == 'POST':
-        form = cadastrar_categoria(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('catalogo:categorias'))
+        Gerencia_categoria.Cria_categoria(request)
+        return HttpResponseRedirect(reverse('catalogo:categorias'))
     else:
-        form = cadastrar_categoria()
-    context = {
-        'form': form
-    }
-    return render(request, "adicionar_categoria.html", context)
+        return render(request, "adicionar_categoria.html")
 
 @login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
+@user_passes_test(check_catalogo, login_url='erro_acesso', redirect_field_name=None)
 def categoria_detalhes(request, slug):
     if request.method == 'POST':
         button = request.POST.get("button")
@@ -57,10 +50,10 @@ def categoria_detalhes(request, slug):
         contexto = {
             'categoria': Categoria.objects.get(slug=slug)
         }
-    return render(request, "categoria_detalhes.html", contexto)
+        return render(request, "categoria_detalhes.html", contexto)
 
 @login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
+@user_passes_test(check_catalogo, login_url='erro_acesso', redirect_field_name=None)
 def produtos(request):
     contexto = {
         'produtos': Produto.objects.all()
@@ -70,23 +63,16 @@ def produtos(request):
         Gerencia_produto.Deleta_produto(delete)
     return render(request, "produtos.html", contexto)
 
-@login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
 def adiciona_produto(request):
     if request.method == 'POST':
-        form = cadastrar_produto(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('catalogo:produtos'))
+        Gerencia_produto.Cria_produto(request)
+        return HttpResponseRedirect(reverse('catalogo:produtos'))
     else:
-        form = cadastrar_produto()
-    context = {
-        'form': form
-    }
-    return render(request, "adicionar_produto.html", context)
+        contexto = {
+            'categorias': Categoria.objects.all()
+        }
+        return render(request, "adicionar_produto.html", contexto)
 
-@login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
 def produto_detalhes(request, slug):
     if request.method == 'POST':
         button = request.POST.get("button")
@@ -103,47 +89,3 @@ def produto_detalhes(request, slug):
             'price': int(produto.price)
         }
         return render(request, "produto_detalhes.html", contexto)
-
-@login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
-def materia(request):
-    contexto = {
-        'materia': Materia.objects.all()
-    }
-    delete = request.POST.get("delete")
-    if delete:
-        Gerencia_materia.Deleta_materia(delete)
-    return render(request, "materia.html", contexto)
-
-@login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
-def adiciona_materia(request):
-    if request.method == 'POST':
-        form = cadastrar_materia(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('catalogo:materia'))
-    else:
-        form = cadastrar_materia()
-    context = {
-        'form': form
-    }
-    return render(request, "adicionar_materia.html", context)
-
-
-@login_required(login_url='sgu:login')
-@user_passes_test(check_catalogo, login_url='sgu:erro_acesso', redirect_field_name=None)
-def materia_detalhes(request, slug):
-    if request.method == 'POST':
-        button = request.POST.get("button")
-        Gerencia_materia.Atualiza_materia(request, slug)
-        if button == "update_continue":
-            return HttpResponseRedirect(reverse('catalogo:materia_detalhes', kwargs={'slug':slug}))
-        elif button == "update":
-            return HttpResponseRedirect(reverse('catalogo:materia'))
-    else:
-        materia = Materia.objects.get(slug=slug)
-        contexto = {
-            'materia': materia,
-        }
-        return render(request, "materia_detalhes.html", contexto)
