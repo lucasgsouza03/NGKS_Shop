1055327db52457b471ef0e83db1dfaa4029c3bdf
lucasgsouza03 <lucas_mardek@hotmail.com>
2018-11-15 01:49:49 -0200

    estoque (merge manual)

--- core/views.py
+++ core/views.py
@@ -11,18 +11,8 @@ from SGU.forms import form_cliente, LoginForm, form_usuario
 from django.urls import reverse, reverse_lazy
 from src.usuario import Gerencia_usuario, Gerencia_permissao
 from django.views.generic import CreateView
-from django.http import HttpResponse
-from django.core.mail import send_mail
-from django.conf import settings
-from .forms import contato_forms
-from django.views.generic import View, TemplateView, CreateView
-from django.contrib import messages
-
 
 # Create your views here.
-
-def check_estoque(request):
-    return 'ESTOQUE' in Gerencia_permissao.Pega_grupo(request)
 
 def check_fluxo(request):
     return 'FLUXO' in Gerencia_permissao.Pega_grupo(request)
@@ -64,7 +54,6 @@ def loginEcommerce(request):
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            usuario = Usuario.objects.get(username=username)
             account = authenticate(username=username, password=password)
             if account is not None:
                 login(request, account)
@@ -81,38 +70,33 @@ def loginEcommerce(request):
         form = LoginForm()
         context = {'form':form}
         return render(request, 'login.html', context)
-
+'''
 class cadastro_cliente(CreateView):
     form_class = form_cliente
     template_name = 'registro.html'
     success_url = reverse_lazy('index')
 
 registro = cadastro_cliente.as_view() 
-
-def contato(request):
-    success = False
-    form = contato_forms(request.POST or None)
-    if form.is_valid():
-        form.send_mail()
-        success = True
+'''
+def registro(request):    
+    if request.method == 'POST':
+        form = form_cliente(request.POST)
+        if form.is_valid():
+            Gerencia_usuario.Cria_cliente(request, form)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return HttpResponseRedirect(reverse('registro'))
     else:
-        messages.error(request, 'Formulário inválido')
-    contexto = {
-        'form': form,
-        'success': success
-    }
-    return render(request, 'contato.html', contexto)
-
+        contexto = {
+            "form" : form_cliente(),
+            "grupos" : Grupos.objects.all(),
+        }            
+        return render(request, "registro.html", contexto)
 
 @login_required(login_url='sgu:login')
 @user_passes_test(check_empresa, login_url='sgu:erro_acesso', redirect_field_name=None)
 def principal(request):
     return render(request, "principal.html")
-
-@login_required(login_url='sgu:login')
-@user_passes_test(check_estoque, login_url='sgu:erro_acesso', redirect_field_name=None)
-def estoque(request):
-    return render(request, "estoque.html")
 
 @login_required(login_url='sgu:login')
 @user_passes_test(check_fluxo, login_url='sgu:erro_acesso', redirect_field_name=None)
