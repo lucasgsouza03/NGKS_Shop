2af7186a84a60a09964f95f391bb741c1deff3a0
lucasgsouza03 <lucas_mardek@hotmail.com>
2018-11-14 23:19:39 -0200

    Revert "Merge branch 'master' into estoque"
    This reverts commit 785ab068857ef41c851d8127fd9d085e8734c267, reversing
    changes made to 578609d736247fe78b2b83e63263367274945a49.

--- core/views.py
+++ core/views.py
@@ -11,13 +11,6 @@ from SGU.forms import form_cliente, LoginForm, form_usuario
 from django.urls import reverse, reverse_lazy
 from src.usuario import Gerencia_usuario, Gerencia_permissao
 from django.views.generic import CreateView
-from django.http import HttpResponse
-from django.core.mail import send_mail
-from django.conf import settings
-from .forms import contato_forms
-from django.views.generic import View, TemplateView, CreateView
-from django.contrib import messages
-
 
 # Create your views here.
 
@@ -64,7 +57,6 @@ def loginEcommerce(request):
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            usuario = Usuario.objects.get(username=username)
             account = authenticate(username=username, password=password)
             if account is not None:
                 login(request, account)
@@ -81,28 +73,28 @@ def loginEcommerce(request):
         form = LoginForm()
         context = {'form':form}
         return render(request, 'login.html', context)
-
+'''
 class cadastro_cliente(CreateView):
     form_class = form_cliente
     template_name = 'registro.html'
     success_url = reverse_lazy('index')
 
 registro = cadastro_cliente.as_view() 
-
-def contato(request):
-    success = False
-    form = contato_forms(request.POST or None)
-    if form.is_valid():
-        form.send_mail()
-        success = True
+'''
+def registro(request):    
+    if request.method == 'POST':
+        form = form_cliente(request.POST)
+        if form.is_valid():
+            Gerencia_usuario.Cria_cliente(request, form)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return HttpResponseRedirect(reverse('registro'))
     else:
-        messages.error(request, 'Formulário inválido')
-    contexto = {
-        'form': form,
-        'success': success
-    }
-    return render(request, 'contato.html', contexto)
-
+        contexto = {
+            "form" : form_cliente(),
+            "grupos" : Grupos.objects.all(),
+        }            
+        return render(request, "registro.html", contexto)
 
 @login_required(login_url='sgu:login')
 @user_passes_test(check_empresa, login_url='sgu:erro_acesso', redirect_field_name=None)
